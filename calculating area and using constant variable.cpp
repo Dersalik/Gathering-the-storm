72f097a8a35e6b95bf49f6d91c59727a6ac497b9
Mahamad sardar <57114424+Dersalik@users.noreply.github.com>
2020-03-03 00:47:57 +0300

    Add files via upload

---
+++ calculating area and using constant variable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ const double Pi= 3.14159 ;
+
+ double radius;
+ cout << "radius bnusa xhushk hiz"<< endl;
+ cin >> radius;
+
+ double area= radius*radius*Pi;
+ cout <<"the area is "<< area;
+ return 0;
+}
