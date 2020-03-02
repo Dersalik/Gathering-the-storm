57e64c37ae153f2d35e8337c1bb5f90c61e52028
Dersalik <57114424+Dersalik@users.noreply.github.com>
2020-03-03 00:20:10 +0300

    Add files via upload

---
+++ Even or odd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+ int number;
+ cout <<"enter a number";
+ cin >> number;
+
+ if (number%2==0)
+ cout <<"this number " << number << " is even";
+ else
+    cout << "this number is odd";
+ return 0;
+
+}
