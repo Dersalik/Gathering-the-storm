92b0a0d4a09d2eeeb58c37c523b65ad2575601ab
Mahamad sardar <57114424+Dersalik@users.noreply.github.com>
2020-03-03 00:38:07 +0300

    Add files via upload

---
+++ squaring numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout <<"Please enter the number you want to square";
+    int number;
+    cin >>number;
+cout << pow(number , 2);
+
+}
